5f9f177ee6a5334169179a6ae7f361bb1e629b79
niharikak <niharikakammili@gmail.com>
2025-03-11 16:42:49 +0530

    add difference example

---
+++ cpractise/assignment-1/difference.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,diff;
+	printf("Enter the value a");
+	scanf("%d",&a);
+	printf("Enter the value b");
+	scanf("%d",&b);
+	diff=a-b;
+	printf("res %d",diff);
+	return 0;
+}
+
