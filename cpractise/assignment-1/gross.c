5470e1c18552ec11b7a7165044616c958ebfde5d
niharikak <niharikakammili@gmail.com>
2025-03-15 22:05:37 +0530

    basic

---
+++ cpractise/assignment-1/gross.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float basic,hra,others,GS;
+	printf("Enter the values");
+	scanf("%f %f %f",&basic,&hra,&others);
+	GS=basic+hra+others;
+	printf("res %f",GS);
+	return 0;
+}
+
+
