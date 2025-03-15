8a86ae05b890baf4bddc57dd1554d1fcf294b33c
niharikak <niharikakammili@gmail.com>
2025-03-15 22:23:56 +0530

    area

---
+++ cpractise/assignment-1/area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int length,width,area;
+	printf("Enter the value");
+	scanf("%d",&length);
+	printf("Enter the value");
+	scanf("%d",&width);
+	area=length*width;
+	printf("res %d",area);
+}
+
