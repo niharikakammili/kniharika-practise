2acc0d1524ee2ff84346a2d44ef037770229963e
niharikakammili <niharikakammili@gmail.com>
2025-06-17 12:22:43 +0530

    test

---
+++ pointer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	int *ptr;
+	ptr=&a;
+	printf("%d \n",a );
+	printf("%d \n",&a );
+	printf("%d \n",ptr);
+	printf("%d \n",*ptr);
+	printf("%d \n",&ptr);
+	return 0;
+}
+
+	
+
