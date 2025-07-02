4c8123438f2381273d45d6c9fc889b4473f3b2a5
niharikakammili <niharikakammili@gmail.com>
2025-07-02 20:19:24 +0530

    test

---
+++ cpractise/assignment8/ternary.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the number \n");
+	scanf("%d",&n);
+	(n%2==0)
+		?printf("number is EVEN")
+		:printf("number is ODD");
+		return 0;
+}
+
+
