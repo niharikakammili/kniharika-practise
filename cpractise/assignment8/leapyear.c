b4dc52446fc8f315937789fa86a5c63f8a4fe38a
niharikakammili <niharikakammili@gmail.com>
2025-07-02 20:29:02 +0530

    test

---
+++ cpractise/assignment8/leapyear.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main
+{
+        int year;
+        printf("enter the year");
+        scanf("%d",&year);
+        (year%4==0 && year%100!=0)?printf("its is leap year"):
+                (year%400==0)?printf("it is a leap year"):printf("coomon");
+        return 0;
+}
+
+
