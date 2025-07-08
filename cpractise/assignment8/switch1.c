4091017e51c4b45e3a48d709e2f74a5377975176
niharikakammili <niharikakammili@gmail.com>
2025-07-08 15:06:13 +0530

    test

---
+++ cpractise/assignment8/switch1.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+	int  ch;
+	printf("enter the chatacter \n");
+	scanf("%c",&ch);
+	switch(ch)
+	{
+		case'a':
+			printf("vowel");
+			break;
+		case'e':
+			printf("vowel");
+			break;
+		case'i':
+			printf("vowel");
+			break;
+		case'o':
+			printf("vowel");
+			break;
+		case'u':
+			printf("vowel");
+			break;
+		case'A':
+			printf("vowe");
+			break;
+		case'E':
+			printf("vowel");
+			break;
+		case'I':
+			printf("vowel");
+			break;
+		case'O':
+			printf("vowel");
+			break;
+		case'U':
+			printf("vowel");
+			break;
+		default:
+			printf("consonants");
+	}
+	return 0;
+}
+
