a0ec50eba7a6203c2ec5af775563c147613b10a6
niharikakammili <niharikakammili@gmail.com>
2025-07-16 16:04:10 +0530

    array

---
+++ cpractise/assignment10/arr5.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int arr[40],n,i,j;
+	printf("enter the number of elements \n");
+	scanf("%d",&n);
+	printf("elements of an array \n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d",i);
+		scanf("%d",&arr[i]);
+	}
+	printf("to find the duplicate elemts \n");
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;i<n;i++)
+		{
+			if(arr[i]==arr[j])
+			{
+				printf("%d",arr[i]);
+			}
+		}
+	}
+	return 0;
+}
+
