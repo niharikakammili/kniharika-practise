a25a2ac61f556323118400af266f3ad27924b7a3
niharikakammili <niharikakammili@gmail.com>
2025-07-16 22:02:53 +0530

    alpha

---
+++ cpractise/assignment11/alphabets.c
@@ -0,0 +1,17 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    printf("Alphabets from a - z are: \n");
+    for(i=97; i<=122; i++)
+    {
+  
+        printf("%c\n", i);
+    }
+
+    return 0;
+}
