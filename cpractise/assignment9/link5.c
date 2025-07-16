253cd03a4493b6f366943b71a25a60c459a62379
niharikakammili <niharikakammili@gmail.com>
2025-07-16 11:40:13 +0530

    link

---
+++ cpractise/assignment9/link5.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+        int data;
+        struct node*next;
+};
+struct node*createnode(int data)
+{
+        struct node*newnode=(struct node*)malloc(sizeof(struct node));
+        newnode ->data=data;
+        newnode ->next=NULL;
+        return newnode;
+}
+void insertatbeginning(struct node**head,int data)
+{
+        struct node*newnode=createnode(data);
+        newnode ->next=*head;
+        *head=newnode;
+}
+void deleteatbegining(struct node**head)
+{
+        struct node*current=*head;
+        *head=current->next;
+        free(current);
+        current->next=NULL;
+}
+
+int main()
+{
+        struct node*head=NULL;
+        head=createnode(10);
+        insertatbeginning(&head,20);
+        insertatbeginning(&head,30);
+	deleteatbegining(&head);
+        struct node*temp=head;
+        while(temp!=NULL)
+        {
+                printf("%d ->",temp ->data);
+                temp=temp ->next;
+        }
+        printf("NULL");
+}
+
