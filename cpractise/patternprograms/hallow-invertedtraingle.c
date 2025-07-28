fabdf68b822eea6d376968635041d35b33bd6956
niharikakammili <niharikakammili@gmail.com>
2025-07-28 14:56:37 +0530

    halloinverted

---
+++ cpractise/patternprograms/hallow-invertedtraingle.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+        int i,j,k,n;
+        printf("enter the number of rows \n");
+        scanf("%d",&n);
+        for(i=n;i>0;i--)
+        {
+                for(k=1;k<=n-i;k++)
+                {
+			printf(" ");
+                       
+                }
+                for(j=1;j<=i;j++)
+                {
+			if(i==n || j==1 || j==i)
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+
+                printf("\n");
+        }
+        return 0;
+}
+
