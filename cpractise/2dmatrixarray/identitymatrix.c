060b0ab3bebbd537dc91688da93e4a68a915767b
niharikakammili <niharikakammili@gmail.com>
2025-09-01 10:55:28 +0530

    identity

---
+++ cpractise/2dmatrixarray/identitymatrix.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+	int a[3][3];
+	int i,j;
+	printf("enter the matrix elements \n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			if(i==j)
+			{
+				a[i][j]=1;
+			}
+			else
+			{
+				a[i][j]=0;
+			}
+		}
+	}
+	printf("after changing to identity matrix \n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d",a[i][j]);
+		}
+	}
+	printf("\n");
+	return 0;
+}
+
+
