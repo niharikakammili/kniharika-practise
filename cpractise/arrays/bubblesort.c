4bf38dbe6c28f93de6b3689a699ec6696a13f7c3
niharikakammili <niharikakammili@gmail.com>
2025-09-13 12:02:05 +0530

    bubblesort

---
+++ cpractise/arrays/bubblesort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[100];
+
+    if (n > 100) {
+        printf("Array size too large (max 100).\n");
+        return 1;
+    }
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Original array: ");
+    printArray(arr, n);
+    bubbleSort(arr, n);
+    printf("Sorted array:   ");
+    printArray(arr, n);
+
+    return 0;
+}
+
