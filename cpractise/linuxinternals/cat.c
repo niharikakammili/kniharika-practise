7a05efc442bf2df687a589096b010121fdee1d2e
niharikakammili <niharikakammili@gmail.com>
2025-10-09 17:58:15 +0530

    cat

---
+++ cpractise/linuxinternals/cat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char ch;
+
+    fp = fopen("file.txt", "r");  
+
+    if (fp == NULL) {
+        printf("Cannot open file.\n");
+        return 1;
+    }
+
+    while ((ch = fgetc(fp)) != EOF) {
+        putchar(ch);  
+    }
+
+    fclose(fp);  
+
+    return 0;
+}
+
