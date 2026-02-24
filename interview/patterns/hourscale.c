eef6303ac5806d49fdce9a7b7841c37f6850bef8
niharikakammili <niharikakammili@gmail.com>
2026-02-24 14:55:48 +0530

    patterns

---
+++ interview/patterns/hourscale.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main()
+{
+    int i, j, k, n;
+
+    printf("Enter the number of rows:\n");
+    scanf("%d", &n);
+
+    for(i = n; i>0; i--)
+    {
+        for(k = 1; k <= n - i; k++)
+        {
+            printf(" ");
+        }
+        for(j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+      for(i = 1; i <= n; i++)
+    {
+        for(k = 1; k <= n - i; k++)
+        {
+            printf(" ");
+        }
+        for(j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
