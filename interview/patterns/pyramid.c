88f14d7b7138fcef024142d8e5e413121add97c1
niharikakammili <niharikakammili@gmail.com>
2026-02-24 12:33:21 +0530

    patterns

---
+++ interview/patterns/pyramid.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int i, j, k, n;
+
+    printf("Enter the number of rows:\n");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+
+        for(k = 1; k <= n - i; k++)
+        {
+            printf("  ");
+        }
+        for(j = 1; j <=2*i-1; j++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
