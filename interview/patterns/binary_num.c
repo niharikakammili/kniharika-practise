b8412d346fb8ffa8f4292eb2b2d347a080f35a46
niharikakammili <niharikakammili@gmail.com>
2026-02-26 10:52:33 +0530

    patterns

---
+++ interview/patterns/binary_num.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int i, j, k, n;
+
+    printf("Enter the number of rows:\n");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        for(k = 1; k <= n - i; k++)
+        {
+            printf(" ");
+        }
+
+        for(j = 1; j <= i; j++)
+        {
+            printf("%d", (i + j) % 2);   // Binary logic
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
