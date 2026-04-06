0b871c395b82a2be218c7f4ec74f1ef4f3a9d574
niharikakammili <niharikakammili@gmail.com>
2026-04-06 17:12:14 +0530

    arrays

---
+++ interview/arrays/linearsearch.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main()
+{
+    int arr[100], i, n, key=0, found=0;
+
+    printf("Enter the number of elements:\n");
+    scanf("%d", &n);
+
+    printf("Enter the key number:\n");
+    scanf("%d", &key);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);  
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] == key)
+        {
+            found = 1;
+            printf("Element found at position %d\n", i + 1);
+            break;
+        }
+    }
+
+    if(found == 0)
+    {
+        printf("Element not found");
+    }
+
+    return 0;
+}
