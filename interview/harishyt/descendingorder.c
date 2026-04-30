1955398c4eb6df8c7bbee037c35438abd08ffbcb
niharikakammili <niharikakammili@gmail.com>
2026-04-30 16:11:34 +0530

    yt

---
+++ interview/harishyt/descendingorder.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+int main()
+{
+        int arr[]={4,5,3,1,2};
+        int n=5,i,j,temp;
+        for(i=0;i<n;i++)
+        {
+                for(j=i+1;j<n;j++)
+                {
+                        if(arr[i]<arr[j])
+                        {
+                                temp=arr[i];
+                                arr[i]=arr[j];
+                                arr[j]=temp;
+                        }
+                }
+        }
+        for(i=0;i<n;i++)
+        {
+                printf("%d",arr[i]);
+        }
+        return 0;
+}
